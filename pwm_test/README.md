8406aea87f16f28378834eb80d4837210ec714d1
David Medin <david@davidmedin.com>
2024-02-14 18:31:47 -0600

    trying an accelerometer. too many comments.

---
+++ pwm_test/README.md
@@ -0,0 +1,3 @@
+https://serval.mythic-beasts.com/~markt/ATmega-timers.html
+https://forum.arduino.cc/t/arduino-mega-pwm-pins-and-frequency/54832
+https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm/
--- pwm_test/pwm_test.ino
+++ pwm_test/pwm_test.ino
@@ -1,3 +1,7 @@
+#include <OPT3101.h>
+#include <Wire.h>
+#include <Arduino.h>
+
 /*
 Motor Magic:
 Black : ground
@@ -9,48 +13,173 @@ Orange : Forward / Reverse. Overcuirrent protection is 0.8A.
   and needs to be re-energized to start again.
 */
 //0 (max speed) (0v) - 1023 (min speed) (5v)
-int speed = 1023-100;
+const int NO_SPEED = 1023;
+const int ALL_SPEED = 0;
+int speed = 1023-200;
 int acc = 100;
 int dir = 1;
 
-int LMotor_PWM = 10;
-int RMotor_PWM = 9;
+// *in is the input from the motor to the computer.
+//        PWM | In  | Dir
+// right : 10    8    11 
+// left :  9    12    13
 
-int LMotor_Dir = 6;
-int RMotor_Dir = 5;
+int RMotor_PWM = 10;
+int LMotor_PWM = 9;
 
-int DistSensor_1 = 2;
-int DistSensor_Out_1 = 3;
+int RMotor_Dir_Pin = 8;
+int LMotor_Dir_Pin = 13;
+bool RMotor_Dir = false;
+bool LMotor_Dir = true;
+
+int RMotor_In = 11;
+int LMotor_In = 12;
+
+// ========== Sensors =================
+/*
+Cables for I2C of Sensor
+Green : data - sda
+Blue : clock - scl
+*/
+OPT3101 sensor;
+
+// Accelerometer pins (the order might be wrong).
+int acc_x_pin = A0;
+int acc_y_pin = A1;
+int acc_z_pin = A2;
+
+int acc_x_offset = 0;
+// Input accelormeter voltage is 3.3 V, so output voltage is sensitive to
+// ~330 mV/g.
+// zero g is about 3.3/2.
+// so, analogRead() should return around 337-338 for zero.
+// 250?
+// ====================================
+
+
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin(); // start I2C things
+
+  // Set register magic to allow PWM to go faster than it should normally.
   // D9 - D10 15.6 kHz 10 bit
   TCCR1A = 0b00000011;  // 10 bit
   TCCR1B = 0b00001001;  // x1 fast pwm mode
 
-  //analogWrite(9, 745);
+  // Tell motors to go speedy through PWM.
   analogWrite(LMotor_PWM, speed);
   analogWrite(RMotor_PWM, speed);
 
-  pinMode(LMotor_Dir, OUTPUT);
-  pinMode(RMotor_Dir, OUTPUT);
-  digitalWrite(LMotor_Dir, HIGH);
-  digitalWrite(RMotor_Dir, LOW);
+  // Go 'forward'
+  pinMode(RMotor_Dir_Pin, OUTPUT);
+  pinMode(LMotor_Dir_Pin, OUTPUT);
+  digitalWrite(RMotor_Dir_Pin, RMotor_Dir);
+  digitalWrite(LMotor_Dir_Pin, LMotor_Dir);
 
-  pinMode(DistSensor_1, INPUT);
-  // pinMode(DistSensor_Out_1, OUTPUT);
-  analogWrite(DistSensor_Out_1, int(float(2.5) * float(1023 / 5)));
+  // Sensor Initialization vvvvvvvvvvvvvvvvv
+  sensor.init();
+  // yoink : https://github.com/pololu/opt3101-arduino/blob/master/examples/Basic/Basic.ino
+  if(sensor.getLastError() != 0) {
+    Serial.print(F("Failed to initialize OPT3101: error "));
+    Serial.println(sensor.getLastError());
+    while (1) {}
+  }
+
+  sensor.setFrameTiming(256); // 256 samples * 0.25 ms + 6% = 68 ms
+  sensor.setChannel(1); // TX1, middle channel
+
+  sensor.setBrightness(OPT3101Brightness::Adaptive);
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+  // unsigned long int ahhh = millis();
+  // Serial.print(ahhh);
+  // Serial.println();
+  // Serial.print((unsigned long int)((float)1000*((float)millis() / (float)1000)));
+  // Serial.println();
+  // Serial.print((float)millis() / (float)1000)
+
+  acc_x_offset = analogRead(acc_x_pin);
 }
 
+void flip_motor_dir() {
+  RMotor_Dir = !RMotor_Dir;
+  LMotor_Dir = !LMotor_Dir;
+  digitalWrite(RMotor_Dir_Pin, RMotor_Dir);
+  digitalWrite(LMotor_Dir_Pin, LMotor_Dir);
+}
+
+unsigned long int mspr = 0;
+int last_rot_sig = 0; // last value recieved from the motor.
+unsigned long int time_ms_of_last_sig = millis();
+
+long int vel_x_g = 0;
+unsigned long int last_loop = 0;
 void loop() {
-  // if(speed+dir*acc > 1023 || speed+dir*acc < 0 ) {
-  //   dir = dir*-1;
+  unsigned long int this_loop = millis();
+  unsigned long int dt = this_loop - last_loop;
+  last_loop = this_loop;
+
+  // sensor.sample();
+
+  // calculate rotations per second.
+  // int r_sig_motor = digitalRead(RMotor_In);
+  // unsigned long int time_now = millis(); 
+  // mspr = (time_now-time_ms_of_last_sig) * 9;
+  // if(r_sig_motor != last_rot_sig) {
+  //   // +1 1/9 rotation.
+    
+  //   time_ms_of_last_sig = time_now;
+  //   last_rot_sig = r_sig_motor;
   // }
 
-  // speed+=dir * acc;
-  // Serial.println( speed );
-  // analogWrite(10, speed);
-  // analogWrite(9, speed);
-  Serial.println( digitalRead(DistSensor_1));
-  delay(200);
+  // Serial.print( (1000.0 / mspr) );
+  // // Serial.print( mspr);
+  // Serial.println();
+
+
+  // if(sensor.distanceMillimeters <= 100 ) {
+  //   analogWrite(LMotor_PWM, NO_SPEED);
+  //   analogWrite(RMotor_PWM, NO_SPEED);
+  //   flip_motor_dir();
+  //   analogWrite(LMotor_PWM, speed);
+  //   analogWrite(RMotor_PWM, speed);
+  //   delay(100);
+  //   analogWrite(LMotor_PWM, NO_SPEED);
+  //   analogWrite(RMotor_PWM, NO_SPEED);
+  //   flip_motor_dir();
+  // }
+  // else {
+  //   analogWrite(LMotor_PWM, speed);
+  //   analogWrite(RMotor_PWM, speed);
+  // }
+
+  int acc_x = analogRead(acc_x_pin);
+  int acc_y = analogRead(acc_y_pin);
+  int acc_z = analogRead(acc_z_pin);
+
+  int acc_x_g = acc_x - acc_x_offset;
+  vel_x_g += acc_x_g * dt;
+
+  float acc_x_mps2 = acc_x_g / 9.8;
+  float vel_x_mps2 = vel_x_g / 9.8;
+  // float acc_x = analogRead(acc_x_pin)/1023.0;
+  // float acc_y = analogRead(acc_y_pin)/1023.0;
+  // float acc_z = analogRead(acc_z_pin)/1023.0;
+  
+  Serial.print("Acc_X:");
+  Serial.print(acc_x_g);
+  Serial.print(",");
+  Serial.print("Vel_X:");
+  Serial.print(vel_x_g);
+  //   Serial.print("Y:");
+  // Serial.print(acc_y);
+  // Serial.print(",");
+  //   Serial.print("Z:");
+  // Serial.print(acc_z);
+  Serial.println();
+
+  // Serial.print(sensor.distanceMillimeters);
+  // Serial.println();
+  delay(40);
 }
